d44b648c06b0fe68ec2a1ad42ecd609c2941857f
Nelli <Student@EPAMYERW0166.moscow.epam.com>
2019-12-19 19:03:52 +0400

    input to get member count

--- src/com/epam/training/Main.java
+++ src/com/epam/training/Main.java
@@ -1,9 +1,12 @@
 package com.epam.training;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Chat.startChat(1);
+        Scanner input = new Scanner(System.in) ;
+        System.out.println("Give the number of members in the group chat");
+        int num=input.nextInt();
+        Chat.startChat(num);
     }
 }
